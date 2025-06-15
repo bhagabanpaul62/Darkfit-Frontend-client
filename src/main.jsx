7d17a6f31baa4e5eaa1155d8ccda0adaec4d4021
bhagabanpaul62 <160720125+bhagabanpaul62@users.noreply.github.com>
2025-06-15 13:29:11 +0530

    fix 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import ContextWraper from "./contextWraper.jsx";
+
+import ContextWraper from "./context.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
